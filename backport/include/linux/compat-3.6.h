8750ec4baab3f4b798f79fd4564be35778af30d2
Luis R. Rodriguez <mcgrof@do-not-panic.com>
2013-04-10 04:35:22 -0700

    compat: backport dma_get_sgtable()
    No architectures yet have their own dma_map_sg ops so we just
    use the commonly developed one for all archs in this
    implementation. We enable this only for 3.2 given that
    this is only needed for dma-buf ops implementations and
    we only port that down to 3.2 so far as well.
    
    mcgrof@frijol ~/linux-stable (git::master)$ git describe --contains d2b7428e
    v3.6-rc1~57^2~3
    
    commit d2b7428eb0caa7c66e34b6ac869a43915b294123
    Author: Marek Szyprowski <m.szyprowski@samsung.com>
    Date:   Wed Jun 13 10:05:52 2012 +0200
    
        common: dma-mapping: introduce dma_get_sgtable() function
    
        This patch adds dma_get_sgtable() function which is required to let
        drivers to share the buffers allocated by DMA-mapping subsystem. Right
        now the driver gets a dma address of the allocated buffer and the kernel
        virtual mapping for it. If it wants to share it with other device (= map
        into its dma address space) it usually hacks around kernel virtual
        addresses to get pointers to pages or assumes that both devices share
        the DMA address space. Both solutions are just hacks for the special
        cases, which should be avoided in the final version of buffer sharing.
    
        To solve this issue in a generic way, a new call to DMA mapping has been
        introduced - dma_get_sgtable(). It allocates a scatter-list which
        describes the allocated buffer and lets the driver(s) to use it with
        other device(s) by calling dma_map_sg() on it.
    
        This patch provides a generic implementation based on virt_to_page()
        call. Architectures which require more sophisticated translation might
        provide their own get_sgtable() methods.
    
        Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
        Reviewed-by: Kyungmin Park <kyungmin.park@samsung.com>
        Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    
    ckmake (only for compat)
    
    1   2.6.24              [  OK  ]
    2   2.6.25              [  OK  ]
    3   2.6.26              [  OK  ]
    4   2.6.27              [  OK  ]
    5   2.6.28              [  OK  ]
    6   2.6.29              [  OK  ]
    7   2.6.30              [  OK  ]
    8   2.6.31              [  OK  ]
    9   2.6.32              [  OK  ]
    10  2.6.33              [  OK  ]
    11  2.6.34              [  OK  ]
    12  2.6.35              [  OK  ]
    13  2.6.36              [  OK  ]
    14  2.6.37              [  OK  ]
    15  2.6.38              [  OK  ]
    16  2.6.39              [  OK  ]
    17  3.0.65              [  OK  ]
    18  3.1.10              [  OK  ]
    19  3.2.38              [  OK  ]
    20  3.3.8               [  OK  ]
    21  3.4.32              [  OK  ]
    22  3.5.7               [  OK  ]
    23  3.6.11              [  OK  ]
    24  3.7.9               [  OK  ]
    25  3.8.0               [  OK  ]
    26  3.9-rc1             [  OK  ]
    
    real    2m39.638s
    user    24m55.205s
    sys     12m59.349s
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- backport/include/linux/compat-3.6.h
+++ backport/include/linux/compat-3.6.h
@@ -26,6 +26,22 @@ int sg_alloc_table_from_pages(struct sg_table *sgt,
 			      unsigned long offset, unsigned long size,
 			      gfp_t gfp_mask);
 
+#define dma_common_get_sgtable LINUX_BACKPORT(dma_common_get_sgtable)
+int
+dma_common_get_sgtable(struct device *dev, struct sg_table *sgt,
+		       void *cpu_addr, dma_addr_t dma_addr, size_t size);
+
+#define dma_get_sgtable_attrs LINUX_BACKPORT(dma_get_sgtable_attrs)
+static inline int
+dma_get_sgtable_attrs(struct device *dev, struct sg_table *sgt, void *cpu_addr,
+		      dma_addr_t dma_addr, size_t size, struct dma_attrs *attrs)
+{
+	return dma_common_get_sgtable(dev, sgt, cpu_addr, dma_addr, size);
+}
+
+#define dma_get_sgtable(d, t, v, h, s) dma_get_sgtable_attrs(d, t, v, h, s, NULL)
+
+
 /**
  * Backports
  *

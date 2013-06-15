66494352c509a842cfb0a676e55252341b65d333
Hauke Mehrtens <hauke@hauke-m.de>
2013-06-15 12:18:30 +0200

    backports: use the given parameter in NETLINK_CB_PORTID()
    NETLINK_CB_PORTID() ignored the given parameter, this was wrong.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- backport/backport-include/linux/netlink.h
+++ backport/backport-include/linux/netlink.h
@@ -6,10 +6,10 @@
 /* this is for patches we apply */
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)
 #define netlink_notify_portid(__notify) (__notify->pid)
-#define NETLINK_CB_PORTID(__skb) NETLINK_CB(cb->skb).pid
+#define NETLINK_CB_PORTID(__skb) NETLINK_CB(__skb).pid
 #else
 #define netlink_notify_portid(__notify) (__notify->portid)
-#define NETLINK_CB_PORTID(__skb) NETLINK_CB(cb->skb).portid
+#define NETLINK_CB_PORTID(__skb) NETLINK_CB(__skb).portid
 #endif
 
 #endif /* __BACKPORT_LINUX_NETLINK_H */

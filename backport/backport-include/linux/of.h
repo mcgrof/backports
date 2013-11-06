40cca26f38f5c48c2aa833fb504f5c523089ac16
Hauke Mehrtens <hauke@hauke-m.de>
2013-11-07 00:26:18 +0100

    backports: always activate the of.h backports
    This removes the KERNEL_HAS_OF_SUPPORT option from backports.
    We can not include linux/of.h on kernel < 2.6.34, because this depends
    on some sparc and powerpc only headers. The backport code should still
    be included in this header file for such kernel versions, because some
    recent drivers are using these functions now on all platforms, but they
    will use the empty implementations then.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- backport/backport-include/linux/of.h
+++ backport/backport-include/linux/of.h
@@ -3,18 +3,13 @@
 
 #include <linux/version.h>
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,34))
-#define KERNEL_HAS_OF_SUPPORT 1
-#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,34)) */
-
-#ifdef CONFIG_OF
-#define KERNEL_HAS_OF_SUPPORT 1
-#endif /* CONFIG_OF */
-
-#ifdef KERNEL_HAS_OF_SUPPORT
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,34) || defined(CONFIG_OF)
 #include_next <linux/of.h>
+#else
+struct device_node;
+#endif
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0))
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)
 #ifdef CONFIG_OF
 extern struct device_node *of_get_child_by_name(const struct device_node *node,
 						const char *name);
@@ -26,9 +21,9 @@ static inline struct device_node *of_get_child_by_name(
 	return NULL;
 }
 #endif /* CONFIG_OF */
-#endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)) */
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0) */
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0))
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)
 #ifndef CONFIG_OF
 static inline struct device_node *of_find_node_by_name(struct device_node *from,
 	const char *name)
@@ -36,8 +31,6 @@ static inline struct device_node *of_find_node_by_name(struct device_node *from,
 	return NULL;
 }
 #endif /* CONFIG_OF */
-#endif
-
-#endif /* KERNEL_HAS_OF_SUPPORT */
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0) */
 
 #endif	/* _COMPAT_LINUX_OF_H */
